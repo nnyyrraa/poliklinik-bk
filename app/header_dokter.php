c90998a124d8ef63f79ef94900575b5b9fa62194
nnyyrraa <tsaniyazhahiran89@gmail.com>
2024-01-08 01:21:23 +0700

    update header dokter

--- app/header_dokter.php
+++ app/header_dokter.php
@@ -26,4 +26,7 @@
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 </head>
