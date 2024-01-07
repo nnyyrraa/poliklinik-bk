4db49cd2692d3479616d6fca59391ce104a9d55d
nnyyrraa <tsaniyazhahiran89@gmail.com>
2024-01-08 01:21:07 +0700

    update footer

--- app/footer.php
+++ app/footer.php
@@ -16,10 +16,19 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Select2 JS -->
+<script src="plugins/select2/js/select2.full.min.js"></script>
+<script>
+  $(document).ready(function() {
+      $('.select2').select2();
+
+      $('.select2bs4').select2({
+        theme: 'bootstrap4'
+      })
+  });
+</script>
 <!-- ChartJS -->
 <script src="plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<!-- <script src="plugins/sparklines/sparkline.js"></script> -->
 <!-- JQVMap -->
 <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
 <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
@@ -36,9 +45,5 @@
 <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<!-- <script src="dist/js/demo.js"></script> -->
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<!-- <script src="dist/js/pages/dashboard.js"></script> -->
 <!-- SweetAlert2 -->
 <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
